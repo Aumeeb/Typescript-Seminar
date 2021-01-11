d1bca4095a1992ce46f3fac64043fc2a22493223
noberk <noberk@users.noreply.github.com>
2021-01-11 16:30:32 +0800

    recommended null check in TS

---
+++ why-we-need-typescript/src/index.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  // Todo
+}
+window.addEventListener('load', function () {
+  window.document.getElementById('addBtn').addEventListener('click', function () {
+    var inputA = document.getElementById('a') // Get instance of inputA
+    var inputB = document.getElementById('b') // Get instance of inputB
+
+    var result = document.getElementById('res') // get instance of Span
+
+    // Todo
+    result.innerHTML = add(inputA.value, inputB.value)
+  })
+})
