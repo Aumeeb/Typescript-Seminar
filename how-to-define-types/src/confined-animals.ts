ef4447a159886fcd5c2e89918193b46fbf1e5736
noberk <noberk@users.noreply.github.com>
2021-01-12 17:31:00 +0800

    rebuild ecosystem.

--- how-to-define-types/src/confined-animals.ts
+++ how-to-define-types/src/confined-animals.ts
@@ -1,12 +1,33 @@
-const buildZoo = () =>
-  ({
-    dog: '🐶',
-    fox: '🦊',
-    lion: '🦁',
-    unicorn: '🦄',
-  } as const)
+const ecosystem = {
+  cactus: '🌵',
+  sheafOfRice: '🌾 ',
+  shamrock: '☘️ ',
+  whiteFlower: '🌸',
+  tulip: '🌷',
+
+  bug: '🐛 ',
+  hamster: '🐹',
+  bear: '🐻',
+} as const
 
 // At the begining Iwan
-class Market {}
-type AnimalObject = typeof buildZoo extends (...args: any) => infer R ? R : never
-type AnimalUnion = AnimalObject[keyof AnimalObject]
+class Vendor {
+  constructor(public bag: any[]) {}
+  pickHerbs(herb: any) {
+    //采药
+    this.bag.push(herb)
+  }
+  info() {
+    //打印
+    console.log(this.bag)
+  }
+}
+
+let me = new Vendor([])
+me.pickHerbs('waa')
+me.pickHerbs('🌲')
+
+console.log(me)
+
+// type AnimalObject = typeof PickHerbs extends (...args: any) => infer R ? R : never
+// type AnimalUnion = AnimalObject[keyof AnimalObject]
