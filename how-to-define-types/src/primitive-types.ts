68d4804fb036afdf35876a772c3d2ecabf7bb428
noberk <noberk@users.noreply.github.com>
2021-01-12 16:24:22 +0800

    update readonly type

--- how-to-define-types/src/primitive-types.ts
+++ how-to-define-types/src/primitive-types.ts
@@ -1,12 +1,15 @@
-const key: unique symbol = Symbol.for('global_a')
-const n1: number = 1
-const hugeNumber: bigint = 1n
-const undefinedd: undefined = undefined
-const n: null = null
-const s1: string = '1'
-const nil: null | undefined = undefined ?? null
+const unique: unique symbol = Symbol.for('global_a')
+let n: number = 1
+let hugeNumber: bigint = 9999999n
+let undefinedd: undefined = undefined
+let nul: null = null
+let s1: string = '1'
+let nil: null | undefined = undefined ?? null
 
-const genericArray: Array<number> = [1]
+let genericArray: readonly [1, 2] = [1, 2] // read only type
+
+let numberArray: any[] = []
+numberArray.push(1)
 
 const fn: () => void = () => void 0
 
