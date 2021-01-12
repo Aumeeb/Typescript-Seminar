7e99995d11995d89ea4d8c29a1869544b44fe2e3
noberk <noberk@users.noreply.github.com>
2021-01-12 18:02:50 +0800

    save

--- how-to-define-types/src/confined-animals.ts
+++ how-to-define-types/src/confined-animals.ts
@@ -1,33 +1,40 @@
 const ecosystem = {
-  cactus: '🌵',
-  sheafOfRice: '🌾 ',
-  shamrock: '☘️ ',
-  whiteFlower: '🌸',
-  tulip: '🌷',
-
-  bug: '🐛 ',
-  hamster: '🐹',
-  bear: '🐻',
-} as const
-
-// At the begining Iwan
-class Vendor {
-  constructor(public bag: any[]) {}
-  pickHerbs(herb: any) {
-    //采药
-    this.bag.push(herb)
+    //1. 首先这是一个原生态圈
+    cactus: '🌵',
+    biCactus: '🌵🌵',
+    sheafOfRice: '🌾 ',
+    shamrock: '☘️ ',
+    whiteFlower: '🌸',
+    tulip: '🌷',
+    hamster: '🐹',
+    bear: '🐻',
+  } as const
+  
+  //2、 我是一个小贩需要采药为生
+  class Vendor {
+    constructor(public bag: any[] = []) {}
+    pickHerbs(herb: any) {
+      //采药
+      this.bag.push(herb)
+    }
+    drop() {
+      this.bag = []
+    }
+    info() {
+      //打印
+      console.log(this.bag)
+    }
   }
-  info() {
-    //打印
-    console.log(this.bag)
-  }
-}
-
-let me = new Vendor([])
-me.pickHerbs('waa')
-me.pickHerbs('🌲')
-
-console.log(me)
-
-// type AnimalObject = typeof PickHerbs extends (...args: any) => infer R ? R : never
-// type AnimalUnion = AnimalObject[keyof AnimalObject]
+  
+  let me = new Vendor()  
+  me.pickHerbs('2323')
+  me.pickHerbs('111')
+  me.pickHerbs('🌲')
+ 
+  console.log(me)
+  me.drop()
+  console.log(me);
+  
+  // type AnimalObject = typeof PickHerbs extends (...args: any) => infer R ? R : never
+  // type AnimalUnion = AnimalObject[keyof AnimalObject]
+  
