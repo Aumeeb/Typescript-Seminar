a610345500a4244496a9c42dc0513c8b1fe5a3bd
noberk <noberk@users.noreply.github.com>
2021-01-17 14:29:11 +0800

    refactor

--- typescript in practice/src/index.ts
+++ typescript in practice/src/index.ts
@@ -1,14 +1,6 @@
-import ecosystem from './server/ecosystem'
-import treasure from './server/treasure'
-import {Nature} from './types'
+import ecosystem from './data/ecosystem'
+import treasure from './data/treasure'
+import type {ExtractSpecificValueFromArray, Nature} from './types'
 
-type ExractSpecificValueFromArray<
-  T extends readonly P[],
-  P extends object,
-  K extends keyof P
-> = T[number][K] extends infer R ? (R extends number | string ? R[] : 'Lack of properties') : never
-
-type Ecosystem = ExractSpecificValueFromArray<typeof ecosystem, Nature & {quantity?: number}, 'quantity'>
-type Ecosystem1 = ExractSpecificValueFromArray<typeof ecosystem, Nature & {quantity?: number}, 'price'>
-type Ecosystem2 = ExractSpecificValueFromArray<typeof ecosystem, Nature & {quantity?: number}, 'symbol'>
-type Treasure = ExractSpecificValueFromArray<typeof treasure, Nature, 'symbol'>
+type Ecosystem = ExtractSpecificValueFromArray<typeof ecosystem, Nature, 'symbol'>
+type Treasure = ExtractSpecificValueFromArray<typeof treasure, Nature, 'symbol'>
--- typescript in practice/src/types.d.ts
+++ typescript in practice/src/types.d.ts
@@ -1,1 +1,6 @@
 export type Nature = {symbol: string; price: number}
+export type ExtractSpecificValueFromArray<
+  T extends readonly P[],
+  P extends object,
+  K extends keyof P
+> = T[number][K] extends infer R ? (R extends number | string ? R : 'Lack of properties') : never
